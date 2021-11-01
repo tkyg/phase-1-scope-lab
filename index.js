5a1287dde85b496ea16f4eb3f842691d7537fd30
tkyg <80114433+tkyg@users.noreply.github.com>
2021-10-31 22:21:12 -0400

    commit tree b3c04b4a162414244de0c9eb851ed6c608ea8bdc

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+// const leastFavoriteCustomer = 'apple'
+// function changeLeastFavroiteCustomer() {
+//  return changeLeastFavoriteCustomer
+// }
+
+
+
+const leastFavoriteCustomer = 'some'
+function changeLeastFavoriteCustomer (){
+    leastFavoriteCustomer = 'some'
+
+}
+
